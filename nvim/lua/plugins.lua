611a5ef537a4bf5f57b23c311553e7bef8d4fb0b
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2025-06-20 22:34:39 +0900

    add hop.nvim plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -60,6 +60,34 @@ local plugins = {
     end,
   },
 
+  -- Jump cursor
+  {
+    "phaazon/hop.nvim",
+    branch = "v2",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>s",
+        function()
+          require("hop").hint_char1()
+        end,
+        mode = "n",
+        desc = "Hop to 1 character",
+      },
+      {
+        "<leader>S",
+        function()
+          require("hop").hint_words()
+        end,
+        mode = "n",
+        desc = "Hop to word",
+      },
+    },
+    config = function()
+      require("hop").setup()
+    end,
+  },
+
   -- UndoTree
   {
     "mbbill/undotree",
