9b15c9e4e7fe6337e4e51a0ec1257015ca4d020a
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2025-06-20 21:18:50 +0900

    chore: add vim-tmux-navigator plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -106,6 +106,18 @@ local plugins = {
       --require("which-key").setup({})
     end,
   },
+
+  -- Vim/tmux間のペイン移動
+  {
+    'christoomey/vim-tmux-navigator',
+    event = 'VeryLazy',
+    keys = {
+      { '<c-h>', '<cmd>TNavigateLeft<cr>',  desc = 'Navigate Left (tmux)' },
+      { '<c-j>', '<cmd>TNavigateDown<cr>',  desc = 'Navigate Down (tmux)' },
+      { '<c-k>', '<cmd>TNavigateUp<cr>',    desc = 'Navigate Up (tmux)' },
+      { '<c-l>', '<cmd>TNavigateRight<cr>', desc = 'Navigate Right (tmux)' },
+    },
+  },
 }
 
 require("lazy").setup(plugins, {})
