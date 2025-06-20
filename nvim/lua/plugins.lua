d839e5cc659168001c1bb7db370dbf541eb8c37a
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2025-06-20 21:43:01 +0900

    chore: add nvim-autopairs plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -109,14 +109,23 @@ local plugins = {
 
   -- Vim/tmux間のペイン移動
   {
-    'christoomey/vim-tmux-navigator',
-    event = 'VeryLazy',
+    "christoomey/vim-tmux-navigator",
+    event = "VeryLazy",
     keys = {
-      { '<c-h>', '<cmd>TNavigateLeft<cr>',  desc = 'Navigate Left (tmux)' },
-      { '<c-j>', '<cmd>TNavigateDown<cr>',  desc = 'Navigate Down (tmux)' },
-      { '<c-k>', '<cmd>TNavigateUp<cr>',    desc = 'Navigate Up (tmux)' },
-      { '<c-l>', '<cmd>TNavigateRight<cr>', desc = 'Navigate Right (tmux)' },
+      { "<c-h>", "<cmd>TNavigateLeft<cr>", desc = "Navigate Left (tmux)" },
+      { "<c-j>", "<cmd>TNavigateDown<cr>", desc = "Navigate Down (tmux)" },
+      { "<c-k>", "<cmd>TNavigateUp<cr>", desc = "Navigate Up (tmux)" },
+      { "<c-l>", "<cmd>TNavigateRight<cr>", desc = "Navigate Right (tmux)" },
     },
+  },
+
+  -- 自動ペアリング
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup {}
+    end,
   },
 }
 
