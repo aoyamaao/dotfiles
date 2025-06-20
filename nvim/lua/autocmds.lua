2570c665fbcdc16b619e3ac8cb1b4360d0a43c39
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2025-06-20 23:54:52 +0900

    chore: configure indentation settings

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -14,3 +14,15 @@ api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove "o"
   end,
 })
+
+-- LuaとVimScriptはインデント幅を2にする
+api.nvim_create_autocmd("FileType", {
+  pattern = { "lua", "vim" },
+  group = custom_autocmds,
+  desc = "Set indent width to 2 for lua and vim files",
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
