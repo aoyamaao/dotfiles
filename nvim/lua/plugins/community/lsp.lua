9d4c133fc645f02e5904d36b8c54133ec0f4d77c
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2025-06-22 18:23:05 +0900

    chore: remove lsp-zero and update lsp

--- nvim/lua/plugins/community/lsp.lua
+++ nvim/lua/plugins/community/lsp.lua
@@ -1,104 +1,134 @@
 return {
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    lazy = true,
-    config = false,
-    dependencies = {
-      { 'nvim-lua/plenary.nvim' },
-    },
-    init = function()
-      -- LSP関連の診断(diagnostic)の記号を無効化
-      vim.diagnostic.config({
-        signs = false,
-        virtual_text = false,
-      })
-    end,
-  },
-
+  -- =================================================================
+  -- 1. Mason
+  -- =================================================================
   {
     'williamboman/mason.nvim',
-    lazy = true,
-    config = true,
+    config = function()
+      require('mason').setup()
+    end,
   },
 
-  -- masonとlspconfigを連携させるためのプラグイン
+  -- =================================================================
+  -- 2. Mason LSP-Config
+  -- =================================================================
   {
     'williamboman/mason-lspconfig.nvim',
-  },
-
-  -- LSP設定
-  {
-    'neovim/nvim-lspconfig',
-    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-    event = { 'BufReadPre', 'BufNewFile' },
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      {
-        'antosha417/nvim-lsp-file-operations',
-        config = true,
-        dependencies = {
-          'nvim-lua/plenary.nvim',
-        },
-      },
-    },
+    dependencies = { 'williamboman/mason.nvim' },
     config = function()
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.on_attach(function(client, bufnr)
-        -- LSPの機能を有効にするキーマッピング
-        lsp_zero.default_keymaps({ buffer = bufnr })
-      end)
-      lsp_zero.extend_lspconfig()
+      -- インストール済みのLSPサーバーを管理し、セットアップの準備をする
       require('mason-lspconfig').setup({
         ensure_installed = { 'lua_ls', 'pyright', 'clangd' },
+      })
+    end,
+  },
+
+  -- =================================================================
+  -- 3. nvim-lspconfig
+  -- =================================================================
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = { 'williamboman/mason-lspconfig.nvim' },
+    config = function()
+      local on_attach = function(client, bufnr)
+        local nmap = function(keys, func, desc)
+          if desc then
+            desc = 'LSP: ' .. desc
+          end
+          vim.keymap.set(
+            'n',
+            keys,
+            func,
+            { buffer = bufnr, noremap = true, silent = true, desc = desc }
+          )
+        end
+        nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+        nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
+        nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+        nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+        nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+        nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        nmap('gl', vim.diagnostic.open_float, 'Line Diagnostics')
+      end
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      require('mason-lspconfig').setup({
         handlers = {
-          lsp_zero.default_setup,
+          function(server_name)
+            require('lspconfig')[server_name].setup({
+              on_attach = on_attach,
+              capabilities = capabilities,
+            })
+          end,
+          ['lua_ls'] = function()
+            require('lspconfig').lua_ls.setup({
+              on_attach = on_attach,
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  diagnostics = { globals = { 'vim' } },
+                },
+              },
+            })
+          end,
         },
       })
     end,
   },
 
-  -- コード補完
+  -- =================================================================
+  -- 4. nvim-cmp (コード補完UI)
+  -- =================================================================
   {
     'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+    event = { 'InsertEnter', 'CmdlineEnter' },
     dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
+      { 'L3MON4D3/LuaSnip', version = 'v2.*', build = 'make install_jsregexp' },
       'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lua',
     },
     config = function()
       local cmp = require('cmp')
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_cmp()
+      local luasnip = require('luasnip')
 
       cmp.setup({
-        -- 補完ソース
-        sources = {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        sources = cmp.config.sources({
           { name = 'nvim_lsp' },
+          { name = 'luasnip' },
           { name = 'buffer' },
           { name = 'path' },
-        },
-        -- キーマッピング
-        mapping = {
+        }),
+        mapping = cmp.mapping.preset.insert({
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
+          ['<C-j>'] = cmp.mapping.select_next_item(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-Space>'] = cmp.mapping.complete(),
-        },
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
       })
-    end,
-  },
-
-  -- スニペット
-  {
-    'L3MON4D3/LuaSnip',
-    version = 'v2.*',
-    dependencies = { 'saadparwaiz1/cmp_luasnip' },
-    -- スニペットエンジンの設定
-    config = function()
-      local luasnip = require('luasnip')
-      luasnip.config.setup({})
     end,
   },
 }
