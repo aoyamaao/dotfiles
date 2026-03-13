e3e4781fab24e56a640c9b57978fc83316df1c91
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2026-03-13 21:44:02 +0900

    feat(markdown): インラインレンダリング環境を構築

---
+++ nvim/lua/plugins/community/markdown.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        vim.list_extend(opts.ensure_installed, { 'markdown', 'markdown_inline' })
+      else
+        opts.ensure_installed = { 'markdown', 'markdown_inline' }
+      end
+    end,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ft = { 'markdown' },
+    opts = {},
+  },
+}
