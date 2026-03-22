57f8806af986f0e4bf6c54176d050a2f8aaa9f7b
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2026-03-23 07:56:58 +0900

    feat: ステータスラインに相対パスを表示

--- nvim/lua/plugins/community/statusline.lua
+++ nvim/lua/plugins/community/statusline.lua
@@ -1,11 +1,25 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'tokyonight-moon',
+  opts = {
+    options = {
+      theme = 'tokyonight-moon',
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = {
+        {
+          'filename',
+          -- 0: ファイル名のみ
+          -- 1: 相対パス (プロジェクトルートからの経路)
+          -- 2: 絶対パス (フルパス)
+          -- 3: 絶対パス (~/から始まる省略形)
+          -- 4: ファイル名と親ディレクトリのみを表示
+          path = 1,
+          file_status = true,
+        },
       },
-    })
-  end,
+    },
+  },
 }
