9a7497b4e0e424d53fbe3eab908f6929891cdffe
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2026-04-15 02:29:02 +0900

    feat: JSONのLSPサポートを追加 (jsonls + schemastore)

--- nvim/lua/plugins/community/lsp.lua
+++ nvim/lua/plugins/community/lsp.lua
@@ -18,14 +18,23 @@ return {
   },
   {
     'neovim/nvim-lspconfig',
-    dependencies = { 'williamboman/mason-lspconfig.nvim' },
+    dependencies = { 'williamboman/mason-lspconfig.nvim', 'b0o/schemastore.nvim' },
     config = function()
       local lsp_core = require('core.lsp')
-      local servers = { 'lua_ls', 'pyright', 'clangd', 'astro', 'ts_ls' }
+      local servers = { 'lua_ls', 'pyright', 'clangd', 'astro', 'ts_ls', 'jsonls' }
 
       vim.lsp.config('*', {
         on_attach = lsp_core.on_attach,
         capabilities = lsp_core.capabilities,
+      })
+
+      vim.lsp.config('jsonls', {
+        settings = {
+          json = {
+            schemas = require('schemastore').json.schemas(),
+            validate = { enable = true },
+          },
+        },
       })
 
       for _, server_name in ipairs(servers) do
