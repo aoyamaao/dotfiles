083f3a8cd17c14e90713ccc9fa4d57fb69129bb1
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2026-06-05 23:37:03 +0900

    feat(nvim): telescopeのpicker設定を整備し検索キーマップを追加
    - Esc=closeを廃止しノーマルモードへの切り替えを確保
    - file_ignore_patternsとfind_commandで検索対象を調整
    - registers/diagnostics/buffers/git_statusにテーマを設定
    - git系pickerを閲覧専用化し破壊的キーを封じる
    - oldfiles/grep_string/resume/registers/keymaps/commandsのキーを追加
    - git status/commitsの閲覧キーを追加

--- nvim/lua/plugins/community/fuzzy-finder.lua
+++ nvim/lua/plugins/community/fuzzy-finder.lua
@@ -13,18 +13,56 @@ return {
 
     telescope.setup({
       defaults = {
-        mappings = {
-          i = {
-            ['<Esc>'] = actions.close,
-          },
-        },
+        file_ignore_patterns = { 'node_modules', '.git/', 'dist/' },
       },
       pickers = {
+        registers = { theme = 'cursor' },
+        diagnostics = { theme = 'ivy' },
+        find_files = {
+          find_command = { 'fd', '--type', 'f', '--hidden', '--exclude', '.git' },
+        },
         buffers = {
+          theme = 'dropdown',
           mappings = {
             i = {
               ['<C-x>'] = actions.delete_buffer,
             },
+            n = {
+              ['<C-x>'] = actions.delete_buffer,
+            },
+          },
+        },
+        -- gitは全て閲覧専用 (破壊的キーを封じる)
+        git_status = {
+          theme = 'ivy',
+          mappings = {
+            i = { ['<Tab>'] = false },
+            n = { ['<Tab>'] = false },
+          },
+        },
+        git_commits = {
+          mappings = {
+            i = { ['<CR>'] = false, ['<C-r>m'] = false, ['<C-r>s'] = false, ['<C-r>h'] = false },
+            n = { ['<CR>'] = false },
+          },
+        },
+        git_bcommits = {
+          mappings = {
+            i = { ['<CR>'] = false },
+            n = { ['<CR>'] = false },
+          },
+        },
+        git_branches = {
+          mappings = {
+            i = {
+              ['<CR>'] = false,
+              ['<C-t>'] = false,
+              ['<C-r>'] = false,
+              ['<C-a>'] = false,
+              ['<C-d>'] = false,
+              ['<C-y>'] = false,
+            },
+            n = { ['<CR>'] = false },
           },
         },
       },
@@ -32,10 +70,21 @@ return {
     telescope.load_extension('fzf')
 
     local map = vim.keymap.set
+    -- 検索・移動
     map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = '[F]ind [F]iles' })
     map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = '[F]ind by [G]rep' })
     map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = '[F]ind [B]uffers' })
     map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = '[F]ind [H]elp' })
     map('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>', { desc = '[F]ind [D]iagnostics' })
+    map('n', '<leader>fo', '<cmd>Telescope oldfiles<cr>', { desc = '[F]ind [O]ldfiles' })
+    map('n', '<leader>fw', '<cmd>Telescope grep_string<cr>', { desc = '[F]ind [W]ord' })
+    map('n', '<leader>fr', '<cmd>Telescope resume<cr>', { desc = '[F]ind [R]esume' })
+    map('n', '<leader>fp', '<cmd>Telescope registers<cr>', { desc = '[F]ind register ([P]aste)' })
+    map('n', '<leader>fk', '<cmd>Telescope keymaps<cr>', { desc = '[F]ind ([K]eymaps)' })
+    map('n', '<leader>fc', '<cmd>Telescope commands<cr>', { desc = '[F]ind ([C]ommands)' })
+    -- git (閲覧専用)
+    map('n', '<leader>gs', '<cmd>Telescope git_status<cr>', { desc = 'Git status' })
+    map('n', '<leader>gc', '<cmd>Telescope git_commits<cr>', { desc = 'Git commits' })
+    map('n', '<leader>gC', '<cmd>Telescope git_bcommits<cr>', { desc = 'Git commits (this file)' })
   end,
 }
