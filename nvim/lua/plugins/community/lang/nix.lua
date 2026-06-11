f7b674a75ff0d4859251aa875ce0aeb327bcda6e
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2026-06-12 04:12:46 +0900

    fix(nvim): nixfmtの標準入力利用時に-引数を渡すように追記

--- nvim/lua/plugins/community/lang/nix.lua
+++ nvim/lua/plugins/community/lang/nix.lua
@@ -1,5 +1,17 @@
 return {
   { 'neovim/nvim-lspconfig', opts = { servers = { nixd = {} } } },
-  { 'stevearc/conform.nvim', opts = { formatters_by_ft = { nix = { 'nixfmt' } } } },
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = { nix = { 'nixfmt' } },
+      formatters = {
+        nixfmt = {
+          command = 'nixfmt',
+          args = { '-' },
+          stdin = true,
+        },
+      },
+    },
+  },
   { 'mfussenegger/nvim-lint', opts = { linters_by_ft = { nix = { 'statix' } } } },
 }
