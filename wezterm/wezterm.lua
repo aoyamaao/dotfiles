50192780817e4d6d337178b37f80f03a70a6329f
aoyamaao <117564484+aoyamaao@users.noreply.github.com>
2026-06-23 20:08:54 +0900

    feat(wezterm): タブとペインの操作用キーバインドを追加

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.font = wezterm.font_with_fallback({
 	"JetBrains Mono",
@@ -15,6 +16,27 @@ config.window_frame = {
 }
 
 config.use_fancy_tab_bar = true
+
+config.keys = {
+	-- タブを閉じる
+	{ key = "w", mods = "CMD", action = act.CloseCurrentTab({ confirm = true }) },
+	-- タブ追加
+	{ key = "t", mods = "CMD", action = act.SpawnTab("CurrentPaneDomain") },
+	-- タブ切り替え
+	{ key = "1", mods = "CMD", action = act.ActivateTabRelative(1) },
+	-- ペインを閉じる
+	{ key = "w", mods = "CMD|SHIFT", action = act.CloseCurrentPane({ confirm = true }) },
+	-- ペイン分割
+	{ key = "d", mods = "CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "d", mods = "CMD|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	-- ペイン拡張
+	{ key = "z", mods = "CMD", action = act.TogglePaneZoomState },
+	-- ペイン移動
+	{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
+}
 
 -- tmux連携
 config.term = "wezterm"
